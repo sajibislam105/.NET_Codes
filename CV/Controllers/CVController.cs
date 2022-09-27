a2bc72432fb32b2120a61f9b0047627b95719a24
sajibislam105 <sajibislam105@gmail.com>
2022-09-28 01:06:17 +0600

    Changing Folder

---
+++ CV/Controllers/CVController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
